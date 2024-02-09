6cd77c58d0059646a92c6cd28a215ffc15e67226
たことぱす <79318163+tacotopasu@users.noreply.github.com>
2024-02-09 17:13:16 +0000

    changed from useNavigate to LinkTo in AboutPage.js

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 function AboutPage() {
-  let navigate = useNavigate();
-
   return (
     <div className="about">
       <h1>About Page</h1>
-      <p><span onClick={() => navigate('/')}>Home</span></p>
+      <Link to="/" class="text-blue-500">Home</Link>
     </div>
   );
 }
